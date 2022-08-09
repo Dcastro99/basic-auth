42a780baffe8cd67616a1751cfe86e701f5a5485
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-08-08 21:08:34 -0700

    added files

--- src/middleware/auth/auth.js
+++ src/middleware/auth/auth.js
@@ -1,32 +1,21 @@
 const SECRET = process.env.SECRET;
-const base64 = require('base-64');
-const bcrypt = require('bcrypt');
-const { Users } = require('../../models/index');
 const jwt = require('jsonwebtoken');
 
-async function validateToken(req, res, next) {
-  if (req.headers.authorization.split(' ')[0] === 'Basic') {
-    let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'sdkjdsljd=']
-    let encodedString = basicHeaderParts.pop();  // sdkjdsljd=
-    let decodedString = base64.decode(encodedString); // "username:password"
-    let [username, password] = decodedString.split(':'); // username, password
-
-    const user = await Users.model.findOne({ where: { username } });
-    const valid = await bcrypt.compare(password, user.password);
-    if (valid) {
-      req.user = user;
-    }
-  }
-
-  if (req.headers.authorization.split(' ')[0] === 'Bearer') {
-    const token = req.headers.authorization.split(' ')[1];
-
-    if (token) {
+function validateToken(req, res, next) {
+  const token = req.headers['authorization'];
+  // console.log("token is : ", token);
+  if (token) {
+    try {
       const user = jwt.verify(token, SECRET);
       req.user = user;
+    } catch (e) {
+      // console.log("Eeek ", e);
+      // Failed to validate the user, oh well
     }
   }
   next();
 }
 
 module.exports = validateToken;
+
+
